ba51a96d12ebbb861cc795c5174d57c0a7668de2
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-03-31 23:22:44 +0530

    Update Views.jsx

--- client/src/components/viewscomponent/Views.jsx
+++ client/src/components/viewscomponent/Views.jsx
@@ -8,7 +8,7 @@ const Views = () => {
       });
       useEffect(() => {           
         // Extracting grouped data for chart
-        axios.get(`http://localhost:4000/api/v1/addnews/getviews`)
+        axios.get(`https://news-app-backend-snowy.vercel.app/api/v1/addnews/getviews`)
         .then(response => {
           const Views = response.data.views;
   
